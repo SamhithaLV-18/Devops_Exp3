965be45635b7e1984009240c50cf936a3c8d62ae
SamhithaLV-18 <samhithalv@gmail>
2026-05-14 20:40:09 +0530

    Updated project files

---
+++ src/test/java/org/test/WebpageTest.java
@@ -0,0 +1,36 @@
+package org.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+
+public class WebpageTest {
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://samhithalv-18.github.io/Devops_Exp3/");
+    }
+
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        System.out.println("Page Title: " + actualTitle);
+
+        String expectedTitle = "Simple website";
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Page title doesn't match!");
+        Assert.assertTrue(actualTitle.contains("website"));
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
